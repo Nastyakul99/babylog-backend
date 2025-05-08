9422b8c6524db56e52076b7fee3ae00ebf67ab25
NovAV <NovAV@lepse.kirov.ru>
2025-05-08 22:07:37 +0300

    Первый коммит

---
+++ src/main/java/kulav/babylog/BabylogApplication.java
@@ -0,0 +1,13 @@
+package kulav.babylog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabylogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabylogApplication.class, args);
+	}
+
+}
